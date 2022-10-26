07e72abc2ebdf74c3d82ac0a0849daf38f3b5f59
omarfaruk7022 <farhanfaruk221@gmail.com>
2022-10-26 12:45:57 +0600

    last

--- index.js
+++ index.js
@@ -9,14 +9,40 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fb3re46.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     await client.connect();
     console.log("db connected");
-    
-   
+    const productsCollection = client.db("smart-coffee").collection("products");
+    const cartCollection = client.db("smart-coffee").collection("cartList");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    app.get("/products/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const product = await productsCollection.findOne(query);
+        res.send(product);
+    })
+    app.post("/addToCart/", async (req, res) => {
+      const newCart = req.body;
+      const result = await cartCollection.insertOne(newCart);
+      res.json(result);
+    })
+    app.get("/cartList", async (req, res) => {
+      const cursor = cartCollection.find({});
+      const cartList = await cursor.toArray();
+      res.send(cartList);
+    });
   } finally {
   }
 }
