5d7364c7d338cdaa04b73990b400f6752041fa89
omarfaruk7022 <farhanfaruk221@gmail.com>
2022-10-29 13:01:34 +0600

    last

--- index.js
+++ index.js
@@ -55,6 +55,12 @@ async function run() {
       const result = await productsCollection.insertOne(newProduct);
       res.json(result);
     })
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.json(result);
+    })
   } finally {
   }
 }
