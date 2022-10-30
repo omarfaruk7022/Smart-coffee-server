4671efddf8913d833a7a52aae6161f9554a42a45
omarfaruk7022 <farhanfaruk221@gmail.com>
2022-10-30 10:56:42 +0600

    last

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ async function run() {
       res.send(products);
     });
     app.get("/products/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const product = await productsCollection.findOne(query);
-        res.send(product);
-    })
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
     app.post("/addToCart/", async (req, res) => {
       const newCart = req.body;
       const result = await cartCollection.insertOne(newCart);
-      res.json(result);
-    })
+      res.send(result);
+    });
     app.get("/cartList/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
@@ -49,28 +49,44 @@ async function run() {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
-      res.json(result);
-    })
+      res.send(result);
+    });
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
       const result = await productsCollection.insertOne(newProduct);
-      res.json(result);
-    })
+      res.send(result);
+    });
     app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await productsCollection.deleteOne(query);
-      res.json(result);
-    })
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const newUser = req.body;
       const result = await usersCollection.insertOne(newUser);
-      res.json(result);
+      res.send(result);
     });
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       const user = await usersCollection.findOne({ email: email });
       res.send({ data: user });
+    });
+    app.get("/cartList", async (req, res) => {
+      const cursor = cartCollection.find({});
+      const cart = await cursor.toArray();
+      res.send(cart);
+    });
+    app.patch("/cartList/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: req.body.status,
+        },
+      };
+      const result = await cartCollection.updateOne(query, updateDoc);
+      res.send(result);
     });
   } finally {
   }
